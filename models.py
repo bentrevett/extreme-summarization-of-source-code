7c4bf69c42a756168e37e795106ad35ea486cedd
bentrevett <bentrevett@gmail.com>
2018-09-05 23:35:29 +0100

    finished first implementation of copy net

--- models.py
+++ models.py
@@ -1,5 +1,8 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
+import random
 
 class AttentionFeatures(nn.Module):
     """
@@ -12,7 +15,7 @@ class AttentionFeatures(nn.Module):
      return Lfeat
     """
     def __init__(self, embedding_dim, k1, w1, k2, w2, w3, dropout):
-        super(AttentionFeatures, self).__init__()
+        super().__init__()
                 
         self.w1 = w1
         self.k1 = k1
@@ -63,7 +66,7 @@ class AttentionWeights(nn.Module):
      return Softmax(Conv1d(Lfeat, K))
     """
     def __init__(self, k2, w3, dropout):
-        super(AttentionWeights, self).__init__()
+        super().__init__()
 
         self.conv1 = nn.Conv1d(k2, 1, w3)
         self.do = nn.Dropout(dropout)
@@ -88,7 +91,7 @@ class AttentionWeights(nn.Module):
     
 class ConvAttentionNetwork(nn.Module):
     def __init__(self, vocab_size, embedding_dim, k1, k2, w1, w2, w3, dropout, pad_idx):
-        super(ConvAttentionNetwork, self).__init__()
+        super().__init__()
 
         self.vocab_size = vocab_size
         self.embedding_dim = embedding_dim
@@ -203,3 +206,137 @@ class ConvAttentionNetwork(nn.Module):
         #outputs = [name len, batch size, vocab dim]
                 
         return outputs
+    
+class CopyAttentionNetwork(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, k1, k2, w1, w2, w3, dropout, pad_idx):
+        super().__init__()
+
+        self.vocab_size = vocab_size
+        self.embedding_dim = embedding_dim
+        self.k1 = k1
+        self.k2 = k2
+        self.w1 = w1
+        self.w2 = w2
+        self.w3 = w3
+        self.dropout = dropout
+        self.pad_idx = pad_idx
+
+        self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=pad_idx)
+        self.do = nn.Dropout(dropout)
+        self.gru = nn.GRU(embedding_dim, k2)
+        self.attn_feat = AttentionFeatures(embedding_dim, k1, w1, k2, w2, w3, dropout)
+        self.attn_weights_alpha = AttentionWeights(k2, w3, dropout)
+        self.attn_weights_kappa = AttentionWeights(k2, w3, dropout)
+        self.conv1 = nn.Conv1d(k2, 1, w3)
+        self.bias = nn.Parameter(torch.ones(vocab_size))
+        
+        n_padding = w1 + w2 + w3 - 3
+        self.padding = torch.zeros(n_padding, 1).fill_(pad_idx).long()
+        
+    def forward(self, bodies, names, tf=None):
+        
+        if tf is None:
+            tf = self.dropout
+            
+        #bodies = [bodies len, batch size]
+        #names = [names len, batch size]  
+        
+        #stores the probabilities generated for each token
+        outputs = torch.zeros(names.shape[0], names.shape[1], self.vocab_size).to(names.device)
+        
+        #outputs = [names len, batch size, vocab dim]
+        
+        #stores the copy attention generated for each token
+        kappas = torch.zeros(names.shape[0], names.shape[1], bodies.shape[0])
+
+        #kappas = [name len, batch size, bodies len]
+
+        #need to pad the function body so after it has been fed through
+        #the convolutional layers it is the same size as the original function body
+        bodies_padded = torch.cat((bodies, self.padding.expand(-1, bodies.shape[1]).to(bodies.device)))
+        
+        #bodies_padded = [bodies len + w1 + w2 + w3 - 3, batch_size]
+        
+        #from now on when we refer to bodies len, we mean the padded version
+        
+        #convert function body tokens into their embeddings
+        emb_b = self.embedding(bodies_padded)
+        
+        #emb_b = [bodies len, batch size, emb dim]
+                
+        #first input to the gru is the first token of the function name
+        #which is a start of sentence token
+        output = names[0]
+            
+        #generate predicted function name tokens one at a time
+        for i in range(1, names.shape[0]):
+                        
+            #initial hidden state is start of sentence token passed through gru
+            #subsequent hidden states from either the previous token predicted by the model
+            #or the ground truth token the model should have predicted
+            _, h_t = self.gru(self.embedding(output).unsqueeze(0))
+
+            #h_t = [1, batch size, k2]
+
+            #computes `k2` features for each token which are scaled by h_t
+            L_feat = self.attn_feat(emb_b, h_t)
+
+            #L_feat = [batch size, k2, bodies len - w1 - w2 + 2]
+
+            #alpha is the attention values for each token in the function body
+            #kappa is the probability that each token in the function body is copied
+            #the second dimension is now equal to the original unpadded `bodies len` size
+            alpha = self.attn_weights_alpha(L_feat)
+            kappa = self.attn_weights_kappa(L_feat)          
+        
+            #alpha = [batch size, bodies len - w1 - w2 - w3 + 3]
+            #kappa = [batch size, bodies len - w1 - w2 - w3 + 3]
+
+            #calculate the weight of predicting by copying from body vs. predicting by guessing from vocab
+            lambd = F.max_pool1d(torch.sigmoid(self.do(self.conv1(L_feat))), alpha.shape[1]).squeeze(2)
+            
+            #emb_b also contains the padding tokens so we slice these off
+            emb_b_slice = emb_b.permute(1, 0, 2)[:, :bodies.shape[0], :]
+
+            #emb_b = [batch_size, bodies len, emb dim]
+
+            #apply the attention to the embedded function body tokens
+            n_hat = torch.sum(alpha.unsqueeze(2) * emb_b_slice, dim=1)
+
+            #n_hat = [batch size, emb dim]
+
+            #E is the embedding layer weights
+            E = self.embedding.weight.unsqueeze(0).expand(bodies.shape[1],-1,-1)
+
+            #E = [batch size, vocab size, emb dim]
+
+            #matrix multiply E and n_hat and apply a bias
+            #n is the probability distribution over the vocabulary for the predicted next token
+            n = torch.bmm(E, n_hat.unsqueeze(2)).squeeze(2) + self.bias.unsqueeze(0).expand(bodies.shape[1], -1)
+            
+            #n = [batch size, vocab size]
+            
+            #store prediction probability distribution in large tensor that holds 
+            #predictions for each token in the function name   
+            outputs[i] = F.softmax(n,dim=1)
+            
+            #store copy probability distribution
+            kappas[i] = kappa
+
+            #with probability of `tf`, use the model's prediction of the next token
+            #as the next token to feed into the model (to become the next h_t)
+            #with probability 1-`tf`, use the actual ground truth next token as
+            #the next token to feed into the model
+            #teacher forcing ratio is equal to dropout during training and 0 during inference
+            if random.random() < tf:
+                
+                #model's predicted token highest value in the probability distribution
+                top1 = n.max(1)[1]
+                output = top1
+                
+            else:
+                output = names[i]
+                
+        #outputs = [name len, batch size, vocab dim]
+                
+        return outputs, kappas
--- run_copy.py
+++ run_copy.py
@@ -2,7 +2,7 @@ import torch
 import torch.optim as optim
 import torch.nn as nn
 
-from torchtext.data import Field
+from torchtext.data import Field, LabelField
 from torchtext.data import TabularDataset
 from torchtext.data import BucketIterator
 
@@ -35,12 +35,11 @@ assert os.path.exists(f'{args.data_dir}/{args.project}_test.json')
 #get available device
 device = torch.device('cuda' if (torch.cuda.is_available() and not args.no_cuda) else 'cpu')
 
-print(device)
-
 #set up fields
 BODY = Field()
 NAME = Field()
-fields = {'name': ('name', NAME), 'body': ('body', BODY)}
+
+fields = {'name': ('name', NAME), 'body': ('body', BODY)} 
 
 #get data from json
 train, test = TabularDataset.splits(
@@ -60,20 +59,21 @@ train_iter, test_iter = BucketIterator.splits(
     (train, test), 
     batch_size=args.batch_size, 
     sort_key=lambda x: len(x.name),
-    repeat=False)
+    repeat=False,
+    device=-1)
 
 #calculate these for the model
 vocab_size = len(BODY.vocab)
 pad_idx = BODY.vocab.stoi['<pad>']
 
 #initialize model
-model = models.ConvAttentionNetwork(vocab_size, args.emb_dim, args.k1, args.k2, args.w1, args.w2, args.w3, args.dropout, pad_idx)
+model = models.CopyAttentionNetwork(vocab_size, args.emb_dim, args.k1, args.k2, args.w1, args.w2, args.w3, args.dropout, pad_idx)
 
 #place on GPU if available
 model = model.to(device)
 
 #initialize optimizer, scheduler and loss function
-criterion = nn.CrossEntropyLoss(ignore_index = pad_idx)
+criterion = nn.BCELoss()
 optimizer = optim.RMSprop(model.parameters())
 
 def train(model, iterator, optimizer, criterion, clip):
@@ -86,12 +86,32 @@ def train(model, iterator, optimizer, criterion, clip):
         
         bodies = batch.body.to(device)
         names = batch.name.to(device)
+
+        copy = torch.zeros(names.shape[0], names.shape[1], bodies.shape[0])
+       
+        _ones = torch.ones(bodies.shape[0])
+        _zeros = torch.zeros(bodies.shape[0])
+
+        for j, (name, body) in enumerate(zip(names, bodies)):
+            for k, token in enumerate(name):
+                copy[j,k,:] = torch.where(bodies[:,k] == token, _ones, _zeros)
         
         optimizer.zero_grad()
         
-        output = model(bodies, names)
-        
-        loss = criterion(output[1:].view(-1, output.shape[2]), names[1:].view(-1))
+        output, kappas = model(bodies, names)
+       
+        output = output[1:].view(-1, output.shape[2])
+        kappas = kappas[1:].view(-1, kappas.shape[2])
+
+        names = names[1:].view(-1,1)
+        one_hot_names = torch.zeros(names.shape[0], output.shape[1])
+        one_hot_names.scatter_(1, names, 1)
+        copy = copy[1:].view(-1, copy.shape[2])
+ 
+        pred = torch.cat((output, kappas), dim=1)
+        target = torch.cat((one_hot_names, copy), dim=1)
+
+        loss = criterion(pred, target)
         
         loss.backward()
         
@@ -100,7 +120,7 @@ def train(model, iterator, optimizer, criterion, clip):
         optimizer.step()
         
         epoch_loss += loss.item()
-        
+     
     return epoch_loss / len(iterator)
 
 def evaluate(model, iterator, criterion):
@@ -115,13 +135,33 @@ def evaluate(model, iterator, criterion):
 
             bodies = batch.body.to(device)
             names = batch.name.to(device)
+            
+            
+            copy = torch.zeros(names.shape[0], names.shape[1], bodies.shape[0])
+       
+            _ones = torch.ones(bodies.shape[0])
+            _zeros = torch.zeros(bodies.shape[0])
 
-            output = model(bodies, names, 0) #set teacher forcing to zero
+            for j, (name, body) in enumerate(zip(names, bodies)):
+                for k, token in enumerate(name):
+                    copy[j,k,:] = torch.where(bodies[:,k] == token, _ones, _zeros)
 
-            loss = criterion(output[1:].view(-1, output.shape[2]), names[1:].view(-1))
+            output, kappas = model(bodies, names, 0) #set teacher forcing to zero
+
+            output = output[1:].view(-1, output.shape[2])
+            kappas = kappas[1:].view(-1, kappas.shape[2])
+
+            one_hot_names = torch.zeros(names.shape[0], output.shape[1])
+            one_hot_names.scatter_(1, names, 1)
+            copy = copy[1:].view(-1, copy.shape[2])
+
+            pred = torch.cat((output, kappas), dim=1)
+            target = torch.cat((one_hot_names, copy), dim=1)
+
+            loss = criterion(pred, target)
 
             epoch_loss += loss.item()
-        
+    
     return epoch_loss / len(iterator)
 
 best_test_loss = float('inf')
@@ -136,6 +176,6 @@ for epoch in range(args.epochs):
     
     if test_loss < best_test_loss:
         best_test_loss = test_loss
-        torch.save(model.state_dict(), f'{args.checkpoints_dir}/{args.project}-conv-model.pt')    
+        torch.save(model.state_dict(), f'{args.checkpoints_dir}/{args.project}-copy-model.pt')    
     
     print(f'| Epoch: {epoch+1:03} | Train Loss: {train_loss:.3f} | Test Loss: {test_loss:.3f} |')
